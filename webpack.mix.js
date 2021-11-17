f4d9dd22052be4b7a7f62ae4b7e6c192d8f5cedc
sabetfar <sabetfar@gmail.com>
2021-11-17 18:28:21 +0330

    admin panel

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,9 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ]);
+    ])
+    .copy('resources/admin/plugins', 'public/admin/js')
+    .copy('resources/admin/dist/img', 'public/admin/dist/img')
+    .copy('resources/admin/dist/js/adminlte.js', 'public/admin/dist/js')
+    .copy('resources/admin/dist/js/pages/dashboard2.js', 'public/admin/dist/js/pages')
+    .css('resources/admin/dist/css/adminlte.min.css', 'public/admin/dist/css');
