efafc6a254f924f64b2ef7849b5cd1b21d22e092
sabetfar <sabetfar@gmail.com>
2021-11-24 17:54:54 +0330

    nothing

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,106 +1,100 @@
-@extends('admin.layout.layout')
-
+@extends('admin.layout.master')
 @section('content')
-{{--@foreach()--}}
-{{--<div class="card-body table-responsive p-0">--}}
-{{--    --}}{{--<button type="button" href="{{route('categories.create')}}" class="btn btn-secondary btn-sm">Create</button>--}}
-{{--    <a href="{{route('categories.create')}}" class="btn btn-secondary btn-sm">Create</a>--}}
-{{--    <table class="table table-striped table-valign-middle">--}}
-{{--        <thead>--}}
-{{--        <tr>--}}
-{{--            <th>Title</th>--}}
-{{--            <th>Parent category</th>--}}
-{{--            --}}{{--<th>Sales</th>--}}
-{{--            <th>More</th>--}}
-{{--        </tr>--}}
-{{--        </thead>--}}
-{{--        <tbody>--}}
-{{--        @foreach($categories as $category)--}}
-{{--            <tr>--}}
-{{--                <td>--}}
-{{--                    {{$category->title}}--}}
-{{--                </td>--}}
-{{--                <td>--}}
-{{--                    {{$category->parent ? $category->parent->title : "-"}}--}}
-{{--                </td>--}}
-{{--                <td>--}}
-{{--                    --}}{{--<a href="#" class="text-muted">--}}
-{{--                        --}}{{--<i class="fas fa-search"></i>--}}
-{{--                    --}}{{--</a>--}}
-{{--                    <button type="button" class="btn btn-primary btn-sm">Edit</button>--}}
-{{--                    <button type="button" class="btn btn-danger btn-sm">Delete</button>--}}
-{{--                </td>--}}
-{{--            </tr>--}}
-{{--            @endforeach--}}
-{{--        </tbody>--}}
-{{--    </table>--}}
-{{--</div>--}}
+    {{--@foreach()--}}
+    {{--<div class="card-body table-responsive p-0">--}}
+    {{--    --}}{{--<button type="button" href="{{route('categories.create')}}" class="btn btn-secondary btn-sm">Create</button>--}}
+    {{--    <a href="{{route('categories.create')}}" class="btn btn-secondary btn-sm">Create</a>--}}
+    {{--    <table class="table table-striped table-valign-middle">--}}
+    {{--        <thead>--}}
+    {{--        <tr>--}}
+    {{--            <th>Title</th>--}}
+    {{--            <th>Parent category</th>--}}
+    {{--            --}}{{--<th>Sales</th>--}}
+    {{--            <th>More</th>--}}
+    {{--        </tr>--}}
+    {{--        </thead>--}}
+    {{--        <tbody>--}}
+    {{--        @foreach($categories as $category)--}}
+    {{--            <tr>--}}
+    {{--                <td>--}}
+    {{--                    {{$category->title}}--}}
+    {{--                </td>--}}
+    {{--                <td>--}}
+    {{--                    {{$category->parent ? $category->parent->title : "-"}}--}}
+    {{--                </td>--}}
+    {{--                <td>--}}
+    {{--                    --}}{{--<a href="#" class="text-muted">--}}
+    {{--                        --}}{{--<i class="fas fa-search"></i>--}}
+    {{--                    --}}{{--</a>--}}
+    {{--                    <button type="button" class="btn btn-primary btn-sm">Edit</button>--}}
+    {{--                    <button type="button" class="btn btn-danger btn-sm">Delete</button>--}}
+    {{--                </td>--}}
+    {{--            </tr>--}}
+    {{--            @endforeach--}}
+    {{--        </tbody>--}}
+    {{--    </table>--}}
+    {{--</div>--}}
 
 
-<section class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1>DataTables</h1>
-            </div>
-        </div>
-    </div><!-- /.container-fluid -->
-</section>
+    {{--<section class="content-header">--}}
+    {{--    <div class="container-fluid">--}}
+    {{--        <div class="row mb-2">--}}
+    {{--            <div class="col-sm-6">--}}
+    {{--                <h1>DataTables</h1>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </div><!-- /.container-fluid -->--}}
+    {{--</section>--}}
 
-
-<section class="content">
-    <div class="container-fluid">
-        <div class="row">
-
-            <div class="card col-sm-12">
-                <div class="card-header">
-                    <h3 class="card-title">Category</h3>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-                    <table id="example1" class="table table-bordered table-striped">
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Parent category</th>
-                            <th>More</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-
-                        @foreach($categories as $category)
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="card col-sm-12">
+                    <div class="card-header" style="display: inline;">
+                        <div>
+                            <h3 class="card-title">Categories</h3>
+                        </div>
+                        <div style="float: inline-end ">
+                            <a href="{{route('categories.create')}}" class="btn btn-secondary btn-sm">Create</a>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
                             <tr>
-                                <td>
-                                    {{$category->title}}
-                                </td>
-                                <td>
-                                    {{$category->parent ? $category->parent->title : "-"}}
-                                </td>
-                                <td>
-                                    {{--<a href="#" class="text-muted">--}}
-                                    {{--<i class="fas fa-search"></i>--}}
-                                    {{--</a>--}}
-                                    <button type="button" class="btn btn-primary btn-sm">Edit</button>
-                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                                </td>
+                                <th>Title</th>
+                                <th>Parent category</th>
+                                <th>More</th>
                             </tr>
-                        @endforeach
-
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach($categories as $category)
+                                <tr>
+                                    <td>
+                                        {{$category->title}}
+                                    </td>
+                                    <td>
+                                        {{$category->parent ? $category->parent->title : "-"}}
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary btn-sm">Edit</button>
+                                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
-                <!-- /.card-body -->
+                <!-- /.card -->
             </div>
-            <!-- /.card -->
+            <!-- /.col -->
         </div>
-        <!-- /.col -->
-    </div>
-    <!-- /.row -->
-    </div>
-
-</section>
-
-
+        <!-- /.row -->
+        </div>
+    </section>
 @endsection
 
 @section('scripts')
@@ -123,17 +117,8 @@
                 "responsive": true, "lengthChange": false, "autoWidth": false,
                 "buttons": ["copy", "csv", "excel", "pdf", "print"]
             }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
         });
+        // const element = document.getElementById("example1_filter");
     </script>
-
 @endsection
 
