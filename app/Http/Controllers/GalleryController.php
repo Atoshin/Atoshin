a00ed8a671cc8338a449bcf74c0ca920c7339d7b
kimiadezfuli <kimiadezfuli@gmail.com>
2021-11-30 12:33:08 +0330

    artist and gallery crud

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gallery;
+use Illuminate\Http\Request;
+
+class GalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $galleries = Gallery:: all();
+        return view('admin.admin.gallery.index', compact('galleries'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.admin.gallery.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        Gallery::query()->create([
+            'name' => $request->name,
+            'bio' => $request->bio,
+            'avatar' => $request->avatar,
+        ]);
+
+
+        return redirect()->route('galleries.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $gallery =Gallery::find($id);
+
+        return view('admin.admin.gallery.edit', compact( 'gallery'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $id)
+    {
+        $gallery =Gallery::find($id);
+        $gallery->name=$request->name;
+        $gallery->bio=$request->bio;
+        $gallery->avatar=$request->avatar;
+        $gallery->save();
+        return redirect()->route('galleries.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $gallery =Gallery::find($id);
+        $gallery->delete();
+        \request()->session()->flash('message', 'deleted successfully');
+        return redirect()->back();
+    }
+}
---
+++ resources/views/admin/admin/gallery/create.blade.php
@@ -0,0 +1,98 @@
+@extends('admin.layout.master')
+@section('content')
+
+    {{--    <div class="card card-info">--}}
+    {{--        <div class="card-header">--}}
+    {{--            <h3 class="card-title">New User</h3>--}}
+    {{--        </div>--}}
+    {{--        <!-- /.card-header -->--}}
+    {{--        <!-- form start -->--}}
+    {{--        <form class="form-horizontal"   method="post"    action="{{route('admin.store')}}">--}}
+    {{--            @csrf--}}
+    {{--            <div class="card-body">--}}
+    {{--                <div class="form-group row">--}}
+    {{--                    <label for="inputEmail3" class="col-sm-2 col-form-label">first_name</label>--}}
+    {{--                    <div class="col-sm-10">--}}
+    {{--                        <input type="text" class="form-control" name="first_name" placeholder="first_name">--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--                <div class="form-group row">--}}
+    {{--                    <label for="inputPassword3" class="col-sm-2 col-form-label">last_name</label>--}}
+    {{--                    <div class="col-sm-10">--}}
+    {{--                        <input type="text" class="form-control" name="last_name" placeholder="last_name">--}}
+    {{--                    </div>--}}
+    {{--                <div class="form-group row">--}}
+    {{--                    <label for="inputPassword3" class="col-sm-2 col-form-label">email</label>--}}
+    {{--                    <div class="col-sm-10">--}}
+    {{--                        <input type="email" class="form-control" name="email" placeholder="email">--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--                <div class="form-group row">--}}
+    {{--                    <label for="inputPassword3" class="col-sm-2 col-form-label">username</label>--}}
+    {{--                    <div class="col-sm-10">--}}
+    {{--                        <input type="text" class="form-control" name="username" placeholder="username">--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--                </div>--}}
+    {{--                <div class="form-group row">--}}
+    {{--                    <label for="inputPassword3" class="col-sm-2 col-form-label">avatar</label>--}}
+    {{--                    <div class="col-sm-10">--}}
+    {{--                        <input type="text" class="form-control" name="avatar" placeholder="avatar">--}}
+    {{--                    </div>--}}
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--            <div class="form-group row">--}}
+    {{--                <label for="inputPassword3" class="col-sm-2 col-form-label">bio</label>--}}
+    {{--                <div class="col-sm-10">--}}
+    {{--                    <input type="text" class="form-control" name="bio" placeholder="bio">--}}
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--            </div>--}}
+    {{--            <!-- /.card-body -->--}}
+    {{--            <div class="card-footer">--}}
+    {{--                <button type="submit" class="btn btn-info">Submit</button>--}}
+    {{--                <button type="submit" class="btn btn-default float-right">Cancel</button>--}}
+    {{--            </div>--}}
+    {{--            <!-- /.card-footer -->--}}
+    {{--        </form>--}}
+    {{--    </div>--}}
+
+    {{--/////--}}
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Edit Info</h3>
+        </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form  method="post"    action="{{route('galleries.store')}}">
+            @csrf
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Bio</label>
+                    <textarea type="text" class="form-control" name="bio" placeholder="Bio"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Avatar</label>
+                    <input type="text" class="form-control" name="avatar" placeholder="Avatar">
+                </div>
+
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+            </div>
+            <!-- /.card-body -->
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-default float-right">Cancel</button>
+            </div>
+        </form>
+    </div>
+
+
+@endsection
