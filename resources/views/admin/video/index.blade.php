c6553a7120205b2deb473e9953da1787d242dc73
kimiadezfuli <kimiadezfuli@gmail.com>
2021-12-01 17:26:39 +0330

    asset show

--- resources/views/admin/video/index.blade.php
+++ resources/views/admin/video/index.blade.php
@@ -41,7 +41,6 @@
 {{--                                        {{ $video->asset->title}}--}}
 {{--                                    </td>--}}
                                     <td>
-
                                         <div class="row">
                                             <div class="col-md-3">
                                                 <a href="{{ route('videos.edit',$video) }}" type="button"
