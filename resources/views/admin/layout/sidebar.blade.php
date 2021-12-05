d4a523ab09e8d36a1a1577832d4534cc471d4513
sabetfar <sabetfar@gmail.com>
2021-12-05 15:17:32 +0330

    sidebar icons edited

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -14,54 +14,61 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
-                <li class="nav-item">
+                <li class="nav-item" style="border-bottom: white 0.7px solid;">
                     <a href="{{route('admin.dashboard')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
+{{--                        <i class="far fa-circle nav-icon"></i>--}}
+                        <i class="fa fa-align-left nav-icon"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{route('categories.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Categories</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('contracts.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Contracts</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('admins.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>admins</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('users.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>users</p>
+                    <a href="{{route('galleries.index')}}" class="nav-link">
+                        <i class="fa fa-building nav-icon"></i>
+                        <p>Galleries</p>
                     </a>
                 </li>
                 <li class="nav-item">
                     <a href="{{route('artists.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>artists</p>
+                        <i class="fa fa-palette nav-icon"></i>
+                        <p>Artists</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{route('galleries.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>gallery</p>
-                    </a>
-                </li>
+
                 <li class="nav-item">
                     <a href="{{route('assets.index')}}" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>assets</p>
+                        <i class="fas fa-paint-brush nav-icon"></i>
+                        <p>Assets</p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="{{route('contracts.index')}}" class="nav-link">
+                        <i class="fas fa-file-contract nav-icon"></i>
+                        <p>Contracts</p>
+                    </a>
+                </li>
+                <li class="nav-item" style="border-bottom: white 0.7px solid;">
+                    <a href="{{route('categories.index')}}" class="nav-link">
+                        <i class="fa fa-cubes nav-icon"></i>
+                        <p>Categories</p>
+                    </a>
+                </li>
+
+                <li class="nav-item" style="border-bottom: white 0.7px solid;">
+                    <a href="{{route('users.index')}}" class="nav-link">
+                        <i class="fa fa-user nav-icon"></i>
+                        <p>Users</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="{{route('admins.index')}}" class="nav-link">
+                        <i class="fas fa-certificate nav-icon"></i>
+                        <p>admins</p>
+                    </a>
+                </li>
+
+
+
 {{--                <li class="nav-header">EXAMPLES</li>--}}
 
 {{--                <li class="nav-item">--}}
