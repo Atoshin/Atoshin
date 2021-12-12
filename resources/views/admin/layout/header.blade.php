e230e1d4add212b9b9616211f50e9ddadfdc25d0
sabetfar <sabetfar@gmail.com>
2021-12-12 18:47:06 +0330

    atoshin logo added

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,6 +1,6 @@
 <!-- Preloader -->
 <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__wobble" src="dist/img/AdminLTELogo.png" height="60" width="60">
+    <img class="animation__wobble" src="{{asset('admin/dist/img/Atoshinlogo.png')}}" height="40" width="120">
 </div>
 
 
