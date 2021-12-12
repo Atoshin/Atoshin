a060e7e4874efafa95139446df82676788d5951f
sabetfar <sabetfar@gmail.com>
2021-12-12 18:18:35 +0330

    image gallery edited

--- resources/views/admin/gallery/show.blade.php
+++ resources/views/admin/gallery/show.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <section class="content-header">
         <div class="container-fluid">
-            <div class="row mb-2">
+            <div class="row mb-2" style="align-items: center;">
                 <div class="col-sm-6">
                     <h1>{{$gallery->name}}</h1>
                 </div>
