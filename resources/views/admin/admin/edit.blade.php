3cdcd247b768a44159f0ff3d23bebde526ec5cbd
sabetfar <sabetfar@gmail.com>
2021-12-13 14:42:32 +0330

    delete password feild in admin edit

--- resources/views/admin/admin/edit.blade.php
+++ resources/views/admin/admin/edit.blade.php
@@ -66,15 +66,15 @@
                     </small>
                     @enderror
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" name="password" value="{{$admin->password}}" placeholder="Password">
-                    @error('password')
-                    <small class="text-danger">
-                        {{$message}}
-                    </small>
-                    @enderror
-                </div>
+{{--                <div class="form-group">--}}
+{{--                    <label for="exampleInputPassword1">Password</label>--}}
+{{--                    <input type="password" class="form-control" name="password" value="{{$admin->password}}" placeholder="Password">--}}
+{{--                    @error('password')--}}
+{{--                    <small class="text-danger">--}}
+{{--                        {{$message}}--}}
+{{--                    </small>--}}
+{{--                    @enderror--}}
+{{--                </div>--}}
 
             </div>
             <!-- /.card-body -->
