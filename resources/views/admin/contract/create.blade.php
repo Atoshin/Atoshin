493a2731b6f7a12ef4486a62c3a32fb3cc098307
sabetfar <sabetfar@gmail.com>
2021-12-22 16:52:35 +0330

    contract uploadre edited

--- resources/views/admin/contract/create.blade.php
+++ resources/views/admin/contract/create.blade.php
@@ -40,7 +40,7 @@
             <!-- /.card-body -->
 
             <div class="card-footer">
-                <button type="submit" id="btnSubmit" class="btn btn-primary">Submit</button>
+                <button type="submit" id="btnSubmit" class="btn btn-primary">Next</button>
 {{--                <button type="submit" class="btn btn-default float-right">Cancel</button>--}}
             </div>
         </form>
