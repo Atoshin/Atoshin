3d8a31b8238e339355493ba9570ff951c5d8b31b
amir-malek <ahmalekpour1999@gmail.com>
2021-12-23 12:51:40 +0330

    connect wallet done

--- resources/views/admin/contract/index.blade.php
+++ resources/views/admin/contract/index.blade.php
@@ -16,70 +16,74 @@
             <div class="row">
                 <div class="card col-sm-12">
                     <div class="card-header">
-                        <a href="{{route('contracts.create',$asset_id)}}" type="button" class="btn btn-success mr-2 float-right"> <i
+                        <a href="{{route('contracts.create',$asset_id)}}" type="button"
+                           class="btn btn-success mr-2 float-right"> <i
                                 class="fa fa-plus mr-2 "></i> Add contract</a>
+                        <button onclick="connectWallet(event)" class="btn btn-success mr-2 float-right"><i
+                                class="fa fa-link mr-2 "></i>Mint asset contracts
+                        </button>
                         <h3 class="card-title">Contract</h3>
 
                     </div>
 
-{{--                    <div class="card col-sm-12">--}}
-{{--                        <div class="card-header">--}}
-{{--                            <h3 class="card-title">Contract</h3>--}}
-{{--                        </div>--}}
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead>
+                {{--                    <div class="card col-sm-12">--}}
+                {{--                        <div class="card-header">--}}
+                {{--                            <h3 class="card-title">Contract</h3>--}}
+                {{--                        </div>--}}
+                <!-- /.card-header -->
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>Contract Number</th>
+                                <th>Hash</th>
+                                <th>Asset</th>
+                                <th>created at</th>
+                                <th>More</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach($contracts as $contract)
                                 <tr>
-                                    <th>Contract Number</th>
-                                    <th>Hash</th>
-                                    <th>Asset</th>
-                                    <th>created at</th>
-                                    <th>More</th>
+                                    <td>
+                                        {{$contract->contract_number}}
+                                    </td>
+                                    <td>
+                                        {{$contract->hash}}
+                                    </td>
+                                    <td>
+                                        {{$contract->asset->title}}
+                                    </td>
+                                    <td>
+                                        {{$contract->created_at}}
+                                    </td>
+                                    <td>
+                                        {{--<a href="#" class="text-muted">--}}
+                                        {{--<i class="fas fa-search"></i>--}}
+                                        {{--</a>--}}
+                                        <div class="row">
+                                            {{--                                                <div class="col-md-3">--}}
+                                            {{--                                                    <a href="{{route('contracts.edit',$contract->id)}}" type="button"--}}
+                                            {{--                                                       class="btn btn-primary "> <i class="fa fa-edit "></i> edit </a>--}}
+                                            {{--                                                </div>--}}
+
+                                        </div>
+                                    </td>
                                 </tr>
-                                </thead>
-                                <tbody>
+                            @endforeach
 
-                                @foreach($contracts as $contract)
-                                    <tr>
-                                        <td>
-                                            {{$contract->contract_number}}
-                                        </td>
-                                        <td>
-                                            {{$contract->hash}}
-                                        </td>
-                                        <td>
-                                            {{$contract->asset->title}}
-                                        </td>
-                                        <td>
-                                            {{$contract->created_at}}
-                                        </td>
-                                        <td>
-                                            {{--<a href="#" class="text-muted">--}}
-                                            {{--<i class="fas fa-search"></i>--}}
-                                            {{--</a>--}}
-                                            <div class="row">
-{{--                                                <div class="col-md-3">--}}
-{{--                                                    <a href="{{route('contracts.edit',$contract->id)}}" type="button"--}}
-{{--                                                       class="btn btn-primary "> <i class="fa fa-edit "></i> edit </a>--}}
-{{--                                                </div>--}}
-
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
+                            </tbody>
+                        </table>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-body -->
                 </div>
-                <!-- /.col -->
+                <!-- /.card -->
             </div>
+            <!-- /.col -->
+        </div>
 
-            <!-- /.row -->
+        <!-- /.row -->
         </div>
 
     </section>
@@ -105,7 +109,20 @@
     <script src="{{asset('admin/js/datatables-buttons/js/buttons.html5.min.js')}}"></script>
     <script src="{{asset('admin/js/datatables-buttons/js/buttons.print.min.js')}}"></script>
     <script src="{{asset('admin/js/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
+    <script src="https://cdn.ethers.io/lib/ethers-5.0.umd.min.js" type="text/javascript">
+    </script>
+    <script>
 
+        async function connectWallet(e) {
+            e.preventDefault()
+            if (window.ethereum) {
+                const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+                await provider.send("eth_requestAccounts", []);
+                const signer = provider.getSigner();
+                const address = await signer.getAddress();
+            }
+        }
+    </script>
     <script>
         $(function () {
             $("#example1").DataTable({
