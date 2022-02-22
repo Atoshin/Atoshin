6077643257ae260ff4c1edf2f392be23aca90fda
amir-malek <ahmalekpour1999@gmail.com>
2022-02-22 13:00:31 +0330

    update user api

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,7 +66,7 @@ class User extends Authenticatable
         return $this->morphOne(Wallet::class, 'walletable');
     }
 
-    public function getUrlAttribute()
+    public function getAvatarAttribute()
     {
         return env('APP_URL') . '/' . $this->avatar;
     }
