db4d97fe0ef3b1e044f769e4b913e744cb24ba27
amir-malek <ahmalekpour1999@gmail.com>
2022-03-05 16:03:23 +0330

    private key updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
 // const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
-const privateKey = '4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356'
+const privateKey = 'f05a7a690cfd04d8304c053f3cc9945e50e9a1068aed2d3f50d0359297f6442c'
 
 module.exports = {
     defaultNetwork: "hardhat",
