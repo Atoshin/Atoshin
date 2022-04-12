3f2ce773a813d389f400b231b58aa501bb281e63
amir-malek <ahmalekpour1999@gmail.com>
2022-04-12 17:35:20 +0430

    transaction added

--- database/migrations/2022_02_28_144626_create_transactions_table.php
+++ database/migrations/2022_02_28_144626_create_transactions_table.php
@@ -14,6 +14,7 @@ class CreateTransactionsTable extends Migration
     public function up()
     {
         Schema::create('transactions', function (Blueprint $table) {
+
             $table->id();
             $table->string('txn_hash');
             $table->string('transactable_type')->nullable();
